f9af8c8534494961ea15521cac8f51b7a8ec4561
Rudi Rocha <rudi.rocha@gmail.com>
2024-10-19 00:33:51 +0100

    Add directory lister

--- symfony/idx-template.nix
+++ symfony/idx-template.nix
@@ -20,6 +20,7 @@
       mkdir composer-home
       export COMPOSER_HOME=./composer-home
 			mkdir "$out"
+      ls "$out"/${dir}
       symfony new --dir="$out"/${dir} --version=${version} ${if webapp == true then "--webapp" else ""} ${if docker == true then "--docker" else ""} ${if cloud == true then "--cloud" else ""} ${if debug == true then "--debug" else ""}
 			mkdir -p "$out"/${dir}/.idx
   		cp ${./dev.nix} "$out"/${dir}/.idx/dev.nix
