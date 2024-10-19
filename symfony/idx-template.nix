68ba5d3efeb0c0db762399df68fb610f71e5d035
Rudi Rocha <rudi.rocha@gmail.com>
2024-10-19 01:34:52 +0100

    Remove dir param usage

--- symfony/idx-template.nix
+++ symfony/idx-template.nix
@@ -19,8 +19,8 @@
     bootstrap = ''
       mkdir composer-home
       export COMPOSER_HOME=./composer-home
-      mkdir -p ${out}/${dir}
-      symfony new --dir=${out}/${dir} --version=${version} ${if webapp == true then "--webapp" else ""} ${if docker == true then "--docker" else ""} ${if cloud == true then "--cloud" else ""} ${if debug == true then "--debug" else ""}
+    
+      symfony new --dir=${dir} --version=${version} ${if webapp == true then "--webapp" else ""} ${if docker == true then "--docker" else ""} ${if cloud == true then "--cloud" else ""} ${if debug == true then "--debug" else ""}
 			mkdir -p ${out}/${dir}/.idx
   		cp ${./dev.nix} ${out}/${dir}/.idx/dev.nix
     '';
